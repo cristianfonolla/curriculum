043ef7a0dd064b6d0eec51f50c81b2c46118ae80
cristian <cristian.fonolla@gmail.com>
2016-11-03 21:48:07 +0100

    SYNC

---
+++ src/Database/seeds/CurriculumSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace School\Curriculum\Database\Seeds;
+
+
+use Illuminate\Database\Seeder;
+
+class CurriculumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
